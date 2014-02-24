0488cfa43a12cbb679f96d0632db50d2e9256a0e
Tara Roys <tlroys@gmail.com>
2014-02-24 11:45:37 -0600

    Does jasmine need to have at least one spec in order to work?
    Log of time taken to
    1. setup project
    2. answer question
    3. all of the commands needed to answer this question
    
     1552  2014-02-24 11:15:47 ls
     1553  2014-02-24 11:15:48 cd ..
     1554  2014-02-24 11:15:48 ls
     1555  2014-02-24 11:15:50 cd ..
     1556  2014-02-24 11:15:50 ls
     1557  2014-02-24 11:16:38 mkdir MondayProjects
     1558  2014-02-24 11:16:40 cd MondayProjects/
     1559  2014-02-24 11:16:40 ls
     1560  2014-02-24 11:17:20 git clone https://github.com/tararoys/Jasmine_Skeleton.git
     1561  2014-02-24 11:17:22 ls
     1562  2014-02-24 11:17:33 mv Jasmine_Skeleton/ javascript_racer
     1563  2014-02-24 11:17:40 git remote rm origin
     1564  2014-02-24 11:17:50 cd javascript_racer/
     1565  2014-02-24 11:17:56 git remote rm origin
     1566  2014-02-24 11:17:58 ls
     1567  2014-02-24 11:19:00 cd spec/
     1568  2014-02-24 11:19:01 ls
     1569  2014-02-24 11:19:05 touch racer.js
     1570  2014-02-24 11:19:06 ls
     1571  2014-02-24 11:19:11 cd ..
     1572  2014-02-24 11:19:14 subl .
     1573  2014-02-24 11:19:20 git remote rm origin
     1574  2014-02-24 11:19:21 ls
     1575  2014-02-24 11:19:23 ls
     1576  2014-02-24 11:19:29 git remote
     1577  2014-02-24 11:22:16 subl .
     1578  2014-02-24 11:23:25 git remote add origin https://github.com/tararoys/javascript_racer.git
     1579  2014-02-24 11:23:32 git push -u origin master
     1580  2014-02-24 11:24:06 gita add README.md
     1581  2014-02-24 11:24:11 git add README.md
     1582  2014-02-24 11:24:12 git commit
     1583  2014-02-24 11:24:23 ls
     1584  2014-02-24 11:24:44 git add README.md
     1585  2014-02-24 11:24:45 git commit
     1586  2014-02-24 11:25:52 ls
     1587  2014-02-24 11:26:04 git push
     1588  2014-02-24 11:26:34 ls
     1589  2014-02-24 11:26:36 cd spec/
     1590  2014-02-24 11:26:37 ls
     1591  2014-02-24 11:29:00 ls /Applications/
     1592  2014-02-24 11:29:11 ls /Library/
     1593  2014-02-24 11:32:22 s
     1594  2014-02-24 11:32:24 ls
     1595  2014-02-24 11:32:26 cd ..
     1596  2014-02-24 11:32:31 ls
     1597  2014-02-24 11:32:46 open SpecRunner.html -a GoogleChrome
     1598  2014-02-24 11:33:14 open SpecRunner.html -a GoogleChrome
     1599  2014-02-24 11:40:37 git history
     1600  2014-02-24 11:40:41 history
     1601  2014-02-24 11:41:18 git add .
     1602  2014-02-24 11:41:23 git status
     1603  2014-02-24 11:41:31 git reset
     1604  2014-02-24 11:41:34 git add -p
     1605  2014-02-24 11:42:09 git add spec/LearnerSpec.js
     1606  2014-02-24 11:42:12 git status
     1607  2014-02-24 11:42:26 git commit  1552  2014-02-24 11:15:47 ls

---
+++ spec/LearnerSpec.js
@@ -0,0 +1,6 @@
+describe("How many questions need answers before I can ship a completed app?", function() {
+  it("Does jasmine need to have at least one spec in order to work?", function(){
+    var answer = true 
+    expect(answer).toEqual(true);
+  })
+});
